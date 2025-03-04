2d50c69c018dde5245bd6317adbb7fae75d43f64
kevin-klein1 <hikevinklein@gmail.com>
2025-03-04 12:17:42 -0500

    Remove flask server run

--- app.py
+++ app.py
@@ -370,8 +370,8 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-port = int(os.getenv('PORT', 5000))   
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+# port = int(os.getenv('PORT', 5000))
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=port)
 
     
